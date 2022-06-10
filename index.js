1a2edc1acc1eaf442b0630d611ad7a93a3ebafde
AkulaPhanidhar <akulaphanidhar30@gmail.com>
2022-06-10 18:26:29 +0530

    first commit

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ let N = 0
 let Y = 0
 
 function Next1() {
-    let hol = document.getElementById("bob")
-    // hol.innerText = parseInt(hol.innerText) + 1
+    let hol = document.getElementById("number")
     N = N + 1
     hol.innerText = N
 }
@@ -12,8 +11,8 @@ let lol = document.getElementById("btn1")
 lol.onclick = Next1
 
 function Next2() {
-    let boy = document.getElementById("bob")
-    boy.style.color = "black"
+    let boy = document.getElementById("number")
+    boy.style.color = "wheat"
     if (boy.innerText == "0") {
         alert("It's Already Zero")
     }
@@ -25,35 +24,35 @@ let bol = document.getElementById("btn2")
 bol.onclick = Next2
 
 let full1 = () => {
-    let bot = document.getElementById("bob")
+    let bot = document.getElementById("number")
     bot.style.color = "red"
 }
 let c1 = document.getElementById("c1")
 c1.onclick = full1
 
 let full2 = () => {
-    let bot = document.getElementById("bob")
+    let bot = document.getElementById("number")
     bot.style.color = "blue"
 }
 let c2 = document.getElementById("c2")
 c2.onclick = full2
 
 let full3 = () => {
-    let bot = document.getElementById("bob")
+    let bot = document.getElementById("number")
     bot.style.color = "green"
 }
 let c3 = document.getElementById("c3")
 c3.onclick = full3
 
 let full4 = () => {
-    let bot = document.getElementById("bob")
+    let bot = document.getElementById("number")
     bot.style.color = "yellow"
 }
 let c4 = document.getElementById("c4")
 c4.onclick = full4
 
 let full5 = () => {
-    let bot = document.getElementById("bob")
+    let bot = document.getElementById("number")
     bot.style.color = "orange"
 }
 let c5= document.getElementById("c5")
